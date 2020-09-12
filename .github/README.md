b655a16fe08a0a7a4078919a6bc957691855c189
Dan Chan <dchan@galileo.io>
2020-09-12 16:53:52 -0400

    adding twitter account two

--- .github/README.md
+++ .github/README.md
@@ -1,1 +1,4 @@
 # twitter-delete
+
+- http://docs.tweepy.org/en/v3.9.0/auth_tutorial.html
+- https://developer.twitter.com/en/apps
--- test.py
+++
@@ -1,6 +0,0 @@
-import re
-status = "Verifying myself: I am brokenthumbs on http://Keybase.io. b0GpF6YUYnPEr7pPP5UPRK0jaFC6CGSwvc65"
-if re.match("Verifying myself: I am .* on http://Keybase.io", status) is not None:
-    print("match")
-else:
-    print("no match")
